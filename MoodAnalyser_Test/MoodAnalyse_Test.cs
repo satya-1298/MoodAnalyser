f989ae2d0676d19ccca5397b49607503bec98bba
unknown <satya-1298>
2023-07-19 08:40:47 +0530

    [Satya]Add : Creating custom Exception for Mood analyser of Null message

--- MoodAnalyser_Test/MoodAnalyse_Test.cs
+++ MoodAnalyser_Test/MoodAnalyse_Test.cs
@@ -31,7 +31,23 @@ namespace MoodAnalyser_Test
             string result = moodAnalyse.AnalyseMood();
             Assert.AreEqual(result, "Happy");
         }
-       
+        [Test]
+        public void GivenNullMessage_ShouldThrowCustomException()
+        {
+            try
+            {
+                //Arrange - it will set up the testing object
+                MoodAnalyse moodAnalyse = new MoodAnalyse(null);
+                //Act - it will perform the actual work what we want test
+                string result = moodAnalyse.AnalyseMood();
+            }
+            catch (MoodAnalyseCustom_Exception ex)
+            {
+                //Assert - which verify the result  by equal actual and expected output
+                Assert.AreEqual(ex.Message, "Message should not be null");
+
+            }
+        }
 
 
 
